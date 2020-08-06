e25585c0a9863711fee850577708476c7d353abf
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-08-06 12:57:41 -0500

    Layout changes, added Passsphrase Generator.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,33 +11,13 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title>ProUtils</q-toolbar-title>
+        <q-btn flat round dense icon="fab fa-github" @click="gotoGitHub" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <LeftDrawer></LeftDrawer>
     </q-drawer>
 
     <q-page-container>
@@ -47,61 +27,25 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { openURL } from 'quasar'
 
 export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
-  data () {
+  name: "MainLayout",
+
+  data() {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
+      leftDrawerOpen: false
+    };
+  },
+
+  components: {
+    LeftDrawer: require("components/LeftDrawer.vue").default
+  },
+
+    methods: {
+    gotoGitHub() {
+      openURL("https://github.com/arothstein/proutils");
   }
-}
+  }
+};
 </script>
